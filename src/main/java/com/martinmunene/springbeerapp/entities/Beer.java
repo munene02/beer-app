056ea5f29d3f455e0c2e1d202bb2845d72f6a321
Martin Munene <munene02@gmail.com>
2023-10-31 17:34:10 +0300

    added data validations, constraints and tests

--- src/main/java/com/martinmunene/springbeerapp/entities/Beer.java
+++ src/main/java/com/martinmunene/springbeerapp/entities/Beer.java
@@ -2,6 +2,9 @@ package com.martinmunene.springbeerapp.entities;
 
 import com.martinmunene.springbeerapp.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -24,10 +27,22 @@ public class Beer {
     private UUID id;
     @Version
     private Integer version;
+
+    @NotBlank
+    @NotNull
+    @Size(max=50)
+    @Column(length = 50)
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotBlank
+    @NotNull
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/com/martinmunene/springbeerapp/repositories/BeerRepositoryTest.java
+++ src/test/java/com/martinmunene/springbeerapp/repositories/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.martinmunene.springbeerapp.repositories;
 
 import com.martinmunene.springbeerapp.entities.Beer;
+import com.martinmunene.springbeerapp.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,13 +22,36 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong(){
+
+        assertThrows(ConstraintViolationException.class, ()->{
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("123456789012345678901234567890123456789012345678901234567890")
+                    .beerStyle(BeerStyle.LAGER)
+                    .upc("12341234")
+                    .price(new BigDecimal("19.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+
+    }
+
+    @Test
     void testSaveBer(){
         Beer savedBeer = beerRepository.save(Beer.builder()
-                        .beerName("My test beer")
-                        .build());
+                .beerName("My test beer")
+                .beerStyle(BeerStyle.LAGER)
+                .upc("12341234")
+                .price(new BigDecimal("19.99"))
+                .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+
+
     }
 
 }
