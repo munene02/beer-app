10c9d6ab40eaa35764d47a0f26e0be3a77e58281
Martin Munene <munene02@gmail.com>
2023-11-01 13:52:38 +0300

    added a mysql proile with its properties

--- src/main/java/com/martinmunene/springbeerapp/entities/Customer.java
+++ src/main/java/com/martinmunene/springbeerapp/entities/Customer.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,7 +23,8 @@ import java.util.UUID;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(length=36, columnDefinition = "varchar", updatable =false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length=36, columnDefinition = "varchar(36)", updatable =false, nullable = false)
     private UUID id;
 
     @NotNull
